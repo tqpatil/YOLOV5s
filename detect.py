94c05b9fcccc821393986382743b6ba2caafd980
Tanishq Patil <trpatil@ucsc.edu>
2025-06-12 00:36:53 -0700

    fix

--- detect.py
+++ detect.py
@@ -104,6 +104,6 @@ if __name__ == "__main__":
         bboxes = non_max_suppression(bboxes, iou_threshold=0.45, threshold=0.25, tolist=False)
         tile = torch.squeeze(tile)
         tile_np = tile[:3, :, :].permute(1, 2, 0).cpu().numpy()  
-        plot_image(tile_np, bboxes, config.labels, filename="figs/1.png")
+        plot_image(tile_np, bboxes, config.labels, filename=f"figs/{i}.png")
 
 
